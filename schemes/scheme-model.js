667b8eb2c8e5956fd69500f300adedef58ee9162
josepaniagua <jpanjr@gmail.com>
2020-06-26 00:49:45 -0700

    add function is returning its own object that was created

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -12,23 +12,28 @@ function findById(schemeId) {
 
 function findSteps(schemeId) {
   return db("schemes as sc")
-      .join("steps as st", "st.scheme_id", "sc.id")
-      .where("st.id", schemeId)
-      .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
-      .orderBy("st.step_number")
+    .join("steps as st", "st.scheme_id", "sc.id")
+    .where("st.id", schemeId)
+    .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+    .orderBy("st.step_number");
 }
 
 function add(scheme) {
-  return db("schemes").insert(scheme)
+  return db("schemes").insert(scheme).then((ownObj) => {
+    return  findById(ownObj[0])
+  })
+
+  // return db("schemes").insert(scheme)
 }
 
+function addStep() {}
 
 module.exports = {
   find,
   findById,
   findSteps,
   add,
-  // addStep,
+  addStep
   // update,
   // remove
 };
