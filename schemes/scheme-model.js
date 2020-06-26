4fa957f0125e369127f108b3a3aeb261cbeb3bd4
josepaniagua <jpanjr@gmail.com>
2020-06-26 00:03:27 -0700

    added findSteps function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,31 @@
-const db = require()
+const db = require("../data/config");
+
+function find() {
+  return db("schemes").select("*");
+}
+
+function findById(schemeId) {
+  return db("schemes as s")
+    .where("s.id", schemeId)
+    .select("*");
+}
+
+function findSteps(schemeId) {
+  return db("schemes as sc")
+      .join("steps as st", "st.scheme_id", "sc.id")
+      .where("st.id", schemeId)
+      .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
+      .orderBy("st.step_number")
+}
+
+
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  // add,
+  // addStep,
+  // update,
+  // remove
+};
